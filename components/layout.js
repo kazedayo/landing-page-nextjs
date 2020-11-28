f12e69f8f8ff1656136c21a8ef42ce02b3e38ef0
Kin Wa Lam <kinwalam@winsons-macbook-pro.local>
2020-11-28 15:25:35 +0800

    responsive margin

--- components/layout.js
+++ components/layout.js
@@ -8,7 +8,7 @@ export const siteTitle = 'Landing Page'
 
 export default function Layout({ children, home }) {
   return (
-    <div className="max-w-xl py-4 mx-24 my-12">
+    <div className="max-w-xl py-4 mx-12 md:mx-24 my-12">
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta
