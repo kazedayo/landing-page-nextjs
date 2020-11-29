cefd6081d7501f088963484ff8fbab7ee3e17e01
Kin Wa Lam <kinwalam@winsons-macbook-pro.local>
2020-11-29 10:16:11 +0800

    add: shadow

--- components/layout.js
+++ components/layout.js
@@ -29,13 +29,13 @@ export default function Layout({ children, home }) {
           <>
             <img
               src="/images/profile.jpg"
-              className="w-32 h-32 rounded-full"
+              className="w-32 h-32 rounded-full shadow-lg"
               alt={name}
             />
             <h1 className="text-4xl leading-5 font-extrabold tracking-tight my-6">{name}</h1>
             <div className="flex flex-row">
-              <SocialIcon className="mx-1" fgColor="#fff" bgColor="#333" url="https://github.com/kazedayo"/>
-              <SocialIcon className="mx-1" fgColor="#fff" url="https://twitter.com/kaze_hayashi97"/>
+              <SocialIcon className="mx-1 rounded-full shadow-md" fgColor="#fff" bgColor="#333" url="https://github.com/kazedayo"/>
+              <SocialIcon className="mx-1 rounded-full shadow-md" fgColor="#fff" url="https://twitter.com/kaze_hayashi97"/>
             </div>
           </>
         ) : (
