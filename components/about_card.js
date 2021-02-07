aa924110026e50d021ada07357c237e42ca674aa
kazedayo <winsonlam@me.com>
2021-02-07 10:42:59 +0800

    update qr

--- components/about_card.js
+++ components/about_card.js
@@ -6,13 +6,15 @@ export default function AboutCard({name, aboutText}) {
             <header className="flex-auto flex-col items-center">
                 <img
                     src="/images/profile.jpg"
-                    className="w-32 h-32 rounded-full shadow-lg"
+                    className="object-cover w-32 h-32 rounded-full shadow-lg"
                     alt={name}
                 />
-                <h1 className="text-4xl leading-5 font-extrabold tracking-tight my-6">{name}</h1>
-                <div className="flex flex-row">
-                    <SocialIcon className="mx-1 rounded-full shadow-md" fgColor="#fff" bgColor="#333" url="https://github.com/kazedayo" />
-                    <SocialIcon className="mx-1 rounded-full shadow-md" fgColor="#fff" url="https://twitter.com/kaze_hayashi97" />
+                <div className="">
+                    <h1 className="text-4xl leading-5 font-extrabold tracking-tight my-6">{name}</h1>
+                    <div className="flex flex-row">
+                        <SocialIcon className="mx-1 rounded-full shadow-md" fgColor="#fff" bgColor="#333" url="https://github.com/kazedayo" />
+                        <SocialIcon className="mx-1 rounded-full shadow-md" fgColor="#fff" url="https://twitter.com/kaze_hayashi97" />
+                    </div>
                 </div>
             </header>
             <main className="max-w-md">
