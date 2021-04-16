ce95b9b55769773bbf52c547c04f89d7bc6f2fd9
kazedayo <winsonlam@me.com>
2021-04-16 16:26:50 +0800

    update content & layout

--- components/about_card.tsx
+++ components/about_card.tsx
@@ -1,28 +1,30 @@
 import { SocialIcon } from 'react-social-icons'
 
-export default function AboutCard({name, aboutText}: {name: string, aboutText: string}) {
+export default function AboutCard({ name, aboutText }: { name: string, aboutText: string }) {
     return (
         <>
-            <header className="flex-auto flex-col items-center">
-                <img
-                    src="/images/profile.jpg"
-                    className="object-cover w-32 h-32 rounded-full shadow-lg"
-                    alt={name}
-                />
-                <div className="">
-                    <h1 className="text-4xl leading-5 font-extrabold tracking-tight my-6">{name}</h1>
-                    <div className="flex flex-row">
-                        <SocialIcon className="mx-1 rounded-full shadow-md" fgColor="#fff" bgColor="#333" url="https://github.com/kazedayo" />
-                        <SocialIcon className="mx-1 rounded-full shadow-md" fgColor="#fff" url="https://twitter.com/kaze_hayashi97" />
+            <div className="px-8 py-8">
+                <header className="flex-auto flex-col items-center">
+                    <img
+                        src="/images/profile.jpg"
+                        className="object-cover w-32 h-32 rounded-full shadow-lg"
+                        alt={name}
+                    />
+                    <div className="">
+                        <h1 className="text-4xl leading-5 font-extrabold tracking-tight my-6">{name}</h1>
+                        <div className="flex flex-row">
+                            <SocialIcon className="mx-1 rounded-full shadow-md" fgColor="#fff" bgColor="#333" url="https://github.com/kazedayo" />
+                            <SocialIcon className="mx-1 rounded-full shadow-md" fgColor="#fff" url="https://twitter.com/kaze_hayashi97" />
+                        </div>
                     </div>
-                </div>
-            </header>
-            <main className="max-w-md">
-                <section className="text-xl font-medium leading-normal py-7">
-                    <p>{aboutText}</p>
-                </section>
-            </main>
-            <img className="shadow-md" src="/images/hehe.svg" width="100px" height="100px"/>
+                </header>
+                <main className="max-w-md">
+                    <section className="text-xl font-medium leading-normal py-7">
+                        <p>{aboutText}</p>
+                    </section>
+                </main>
+                <img className="shadow-md" src="/images/hehe.svg" width="100px" height="100px" />
+            </div>
         </>
     )
 }
--- components/showcase_panel.tsx
+++ components/showcase_panel.tsx
@@ -4,16 +4,33 @@ import GithubLink from './github_link'
 export default function ShowcasePanel() {
     return (
         <>
-            <h1 className="text-2xl font-bold leading-8">Personal Projects</h1>
-            <div className="py-2 max-w-screen-xl">
-                <div className="flex flex-row items-center space-x-2">
-                    <h2 className="text-xl font-medium">hkGalden</h2><GithubLink repo="kazedayo/hkgalden_flutter" />
+            <div className="px-8 py-8 md:overflow-y-scroll">
+                <h1 className="text-2xl font-bold leading-8">Personal Projects</h1>
+                <div className="py-2 max-w-screen-xl">
+                    <div className="flex flex-row items-center space-x-2">
+                        <h2 className="text-xl font-medium">hkGalden</h2><GithubLink repo="kazedayo/hkgalden_flutter" />
+                    </div>
+                    <p className="text-gray-400">Originally an iOS client for a small Hong Kong forum hkGalden, now in Flutter.</p>
+                    <div className="flex flex-wrap overflow-hidden">
+                        <ImageContainer src="/images/project1/mockup1.png" />
+                        <ImageContainer src="/images/project1/mockup2.png" />
+                        <ImageContainer src="/images/project1/mockup3.png" />
+                    </div>
                 </div>
-                <p className="text-gray-400">Originally an iOS client for a small Hong Kong forum hkGalden, now in Flutter.</p>
-                <div className="flex flex-wrap overflow-hidden">
-                    <ImageContainer src="/images/project1/mockup1.png" />
-                    <ImageContainer src="/images/project1/mockup2.png" />
-                    <ImageContainer src="/images/project1/mockup3.png" />
+                <h1 className="text-2xl font-bold leading-8">Contributed Open Souce Projects</h1>
+                <div className="py-2">
+                    <div className="py-2">
+                        <div className="flex flex-row items-center space-x-2">
+                            <h2 className="text-xl font-medium">Moya</h2><GithubLink repo="Moya/Moya" />
+                        </div>
+                        <p className="text-gray-400">Network Abstraction Layer for Swift based on Alamofire.</p>
+                    </div>
+                    <div className="py-2">
+                        <div className="flex flex-row items-center space-x-2">
+                            <h2 className="text-xl font-medium">Holodex</h2><GithubLink repo="RiceCakess/Holodex" />
+                        </div>
+                        <p className="text-gray-400">An PWA that centralizes Virtual YouTubers' livestreams/VODs/Clips.</p>
+                    </div>
                 </div>
             </div>
         </>
