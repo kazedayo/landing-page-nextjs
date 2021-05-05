5d2d74e3375c44bbb24ab1ffbbda6fed6ea641c5
kazedayo <winsonlam@me.com>
2021-05-05 17:13:11 +0800

    update site content

--- components/showcase_panel.tsx
+++ components/showcase_panel.tsx
@@ -6,15 +6,22 @@ export default function ShowcasePanel() {
         <>
             <div className="px-8 md:px-16 py-8 md:py-16 md:overflow-auto">
                 <h1 className="text-2xl font-bold leading-8">Personal Projects</h1>
-                <div className="py-2 max-w-screen-xl">
+                <div className="py-2 max-w-screen-lg">
                     <div className="flex flex-row items-center space-x-2">
                         <h2 className="text-xl font-medium">hkGalden</h2><GithubLink repo="kazedayo/hkgalden_flutter" />
                     </div>
-                    <p className="text-gray-400">Originally an iOS client for a small Hong Kong forum hkGalden, now in Flutter.</p>
+                    <p className="text-gray-400">Originally an iOS client for a small Hong Kong forum hkGalden written in Swift, now rewritten in Flutter.</p>
                     <div className="flex flex-wrap overflow-hidden">
                         <ImageContainer src="/images/project1/mockup1.png" />
                         <ImageContainer src="/images/project1/mockup2.png" />
                         <ImageContainer src="/images/project1/mockup3.png" />
+                    </div>
+                    <div className="flex flex-row items-center space-x-2">
+                        <h2 className="text-xl font-medium">HoloSchedule</h2><GithubLink repo="kazedayo/hololive_app" />
+                    </div>
+                    <p className="text-gray-400">Simple app to keep track of stream schedules of Hololive members and ability to subscribe and receive notifications when a stream is live. Written in Flutter</p>
+                    <div className="flex flex-wrap overflow-hidden">
+                        <ImageContainer src="/images/project2/preview.png" />
                     </div>
                 </div>
                 <h1 className="text-2xl font-bold leading-8">Contributed Open Souce Projects</h1>
