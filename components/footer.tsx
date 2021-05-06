9ba18fc0ef7a4dae8cb4b72c276359facfe00704
kazedayo <winsonlam@me.com>
2021-05-06 13:42:10 +0800

    use tailwindcss 2.1's backdrop blur

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer({children}:{children:React.ReactNode}) {
     return (
-        <div className="fixed p-4 bottom-0 inset-x-0 w-full backdrop-blur">
+        <div className="fixed p-4 bottom-0 inset-x-0 w-full backdrop-filter backdrop-blur">
             <p className="text-xs text-center text-gray-400">{children}</p>
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,10 @@ module.exports = {
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
-    backdropFilter: {
-      'blur': 'blur(1rem)'
-    }
   },
   variants: {
     extend: {},
   },
   plugins: [
-    require('tailwindcss-filters'),
   ],
 }
